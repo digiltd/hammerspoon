71a3e419b60dc75f3aa28d7f349468b98a8f8ece
Sam Turner <sam@digi.ltd.uk>
2017-01-03 13:58:11 +0000

    added § for iTerm

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ k = hs.hotkey.modal.new({}, 'F17')
 
 -- Hyper-key for all the below are setup somewhere... Usually Keyboard Maestro or similar. Alfred doesn't handle them very well, so will set up separate bindings for individual apps below.
 
-hyperBindings = {'c','m','a','b','d','e','f','g','h','i','j','k','l','m','n','p','q','r','t','1','2','3','4','5','6','7','8','9','0','d','g','s','f','TAB','v','b','O','-','s'}
+hyperBindings = {'§','c','m','a','b','d','e','f','g','h','i','j','k','l','m','n','p','q','r','t','1','2','3','4','5','6','7','8','9','0','d','g','s','f','TAB','v','b','O','-','s'}
 
 for i,key in ipairs(hyperBindings) do
   k:bind({}, key, nil, function() hs.eventtap.keyStroke({'cmd','alt','shift','ctrl'}, key)
