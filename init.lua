440dc2de980f9173de39fbf93f66330568b34a66
Sam Turner <sam@digi.ltd.uk>
2018-01-05 13:00:01 +0000

    rough windo switcher

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
 require('hyper-applauncher')
 require('hyper-external')
 require('window-management')
+require('window-switcher')
+
+
 require('config-watcher')
+
+
+hs.hotkey.bind('alt','tab','Next window',function()switcher:next()end)
+hs.hotkey.bind('alt-shift','tab','Prev window',function()switcher:previous()end)
 
 -- macOS notification when loaded
 hs.notify.new({title='Hammerspoon', informativeText='Everything is 🍑'}):send()
